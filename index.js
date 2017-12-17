38a0da259ef40b0ec4c619c012637f795be486ff
Rhys Williams <rwillcolumbia@gmail.com>
2017-12-17 22:19:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function happyHolidays(){
   document.write('Happy holidays!');
 }
 
+function happyHolidaysTo(){
+  document.write('Happy holidays, ${name}!')
+}
+function happyHolidayTo(){
+  document.write('Happy ${holdiay}, ${name}!')
+}
